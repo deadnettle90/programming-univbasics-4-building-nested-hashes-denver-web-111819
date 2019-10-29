8e640582ea398432caa9093dbb1602e6e3b60d06
Danielle Matton <danimatton@gmail.com>
2019-10-29 03:36:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,10 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      
+	      :rent_one => "",
+	      :rent_two => "",
+	      :rent_three => "",
+	      :rent_four => ""
 	    }
 	  }
 	}
