54cc5997b695126e742f73a2fb820a7e998e880d
Danielle Matton <danimatton@gmail.com>
2019-10-29 03:46:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,7 +60,7 @@ def monopoly_with_fourth_tier
 	      :four_pieces_owned => 200,
 	    },
 	    :names => {
-	      :reading_railroad => { :mortgage_value => "$10" },
+	      :reading_railroad => { :mortgage_value => "$100" },
 	      :pennsylvania_railroad => { :mortgage_value => "$10" },
 	      :b_and_o_railroad => { :mortgage_value => "$10" },
 	      :shortline_railroad => { :mortgage_value => "$10" }
